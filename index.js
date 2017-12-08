5fb4dbca0216e55056bc5d4fa971e8ea926c581e
jdlrobson <jdlrobson@gmail.com>
2017-12-08 10:23:40 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	CreateAccountPage: require( './createaccount.page' ),
+	EditPage: require( './edit.page' ),
+	HistoryPage: require( './history.page' ),
+	Page: require( './page' ),
+	PreferencesPage: require( './preferences.page' ),
+	UserLoginPage: require( './userlogin.page' )
+};
